54d7878e0ce1036fab0a4da23fea020951e90158
reemhashir <rmuhamme@andrew.cmu.edu>
2024-09-05 05:46:13 -0400

    attempt to pass testing errors on sonarCloud

--- test/public/src/admin/settings/navigation.js
+++ test/public/src/admin/settings/navigation.js
@@ -1,4 +1,7 @@
-// navigation.test.js
+'use strict';
+
+/* global jest, beforeAll, test, expect, document */
+
 const $ = require('jquery');
 require('jquery-ui/ui/widgets/draggable');
 require('jquery-ui/ui/widgets/droppable');
@@ -10,55 +13,56 @@ jest.mock('benchpress');
 jest.mock('alerts');
 
 describe('navigation', () => {
-    let navigation;
-    let ajaxify;
+	let navigation;
+	let ajaxify;
 
-    beforeAll(() => {
-        document.body.innerHTML = `
-            <ul id="available">
-                <li><div class="drag-item"></div></li>
-            </ul>
-            <ul id="active-navigation"></ul>
-        `;
+	beforeAll(() => {
+		document.body.innerHTML = `
+			<ul id="available">
+				<li><div class="drag-item"></div></li>
+			</ul>
+			<ul id="active-navigation"></ul>
+		`;
 
-        ajaxify = { data: { available: [] } };
-        global.ajaxify = ajaxify;
+		ajaxify = { data: { available: [] } };
+		global.ajaxify = ajaxify;
 
-        navigation = require('./navigation');
-    });
+		navigation = require('./navigation');
+	});
 
-    test('should initialize navigation', () => {
-        const initDraggable = jest.spyOn(navigation, 'initDraggable');
-        const initSortableAndDroppable = jest.spyOn(navigation, 'initSortableAndDroppable');
-        const initIconPicker = jest.spyOn(navigation, 'initIconPicker');
-        const initDropdownToggle = jest.spyOn(navigation, 'initDropdownToggle');
-        const initClickHandlers = jest.spyOn(navigation, 'initClickHandlers');
+	test('should initialize navigation', () => {
+		const initDraggable = jest.spyOn(navigation, 'initDraggable');
+		const initSortableAndDroppable = jest.spyOn(navigation, 'initSortableAndDroppable');
+		const initIconPicker = jest.spyOn(navigation, 'initIconPicker');
+		const initDropdownToggle = jest.spyOn(navigation, 'initDropdownToggle');
+		const initClickHandlers = jest.spyOn(navigation, 'initClickHandlers');
 
-        navigation.init();
+		navigation.init();
 
-        expect(initDraggable).toHaveBeenCalled();
-        expect(initSortableAndDroppable).toHaveBeenCalled();
-        expect(initIconPicker).toHaveBeenCalled();
-        expect(initDropdownToggle).toHaveBeenCalled();
-        expect(initClickHandlers).toHaveBeenCalled();
-    });
+		expect(initDraggable).toHaveBeenCalled();
+		expect(initSortableAndDroppable).toHaveBeenCalled();
+		expect(initIconPicker).toHaveBeenCalled();
+		expect(initDropdownToggle).toHaveBeenCalled();
+		expect(initClickHandlers).toHaveBeenCalled();
+	});
 
-    test('should initialize draggable items', () => {
-        navigation.initDraggable();
+	test('should initialize draggable items', () => {
+		navigation.initDraggable();
 
-        const draggableItems = $('#available').find('li .drag-item');
-        expect(draggableItems.draggable('instance')).toBeTruthy();
-    });
+		const draggableItems = $('#available').find('li .drag-item');
+		expect(draggableItems.draggable('instance')).toBeTruthy();
+	});
 
-    test('should initialize sortable and droppable', () => {
-        navigation.initSortableAndDroppable();
+	test('should initialize sortable and droppable', () => {
+		navigation.initSortableAndDroppable();
 
-        const sortable = $('#active-navigation').sortable('instance');
-        const droppable = $('#active-navigation').droppable('instance');
+		const sortable = $('#active-navigation').sortable('instance');
+		const droppable = $('#active-navigation').droppable('instance');
 
-        expect(sortable).toBeTruthy();
-        expect(droppable).toBeTruthy();
-    });
+		expect(sortable).toBeTruthy();
+		expect(droppable).toBeTruthy();
+	});
 
-    // Add more tests for other initialization functions if needed
+	// Add more tests for other initialization functions if needed
 });
+
