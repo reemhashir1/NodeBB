52f128c5c4d6f35bef0496c1a74c2ae228eab3e7
reemhashir <rmuhamme@andrew.cmu.edu>
2024-09-05 10:21:11 -0400

    working on sonarcloud

--- test/public/src/admin/settings/navigationtest.js
+++ test/public/src/admin/settings/navigationtest.js
@@ -1,162 +1,59 @@
-'use strict';
-
-const async = require('async');
 const assert = require('assert');
-const db = require('../mocks/databasemock');
 const sinon = require('sinon');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
+const $ = require('jquery');
+const { JSDOM } = require('jsdom');
+const navigation = require('../public/src/admin/settings/navigation');
 
-// Set the path to the navigation module
-const navigationModulePath = 'public/src/admin/settings/navigation.js';
+// Set up a DOM environment for testing
+const { window } = new JSDOM('<!doctype html><html><body><div id="available"><ul></ul></div><div id="active-navigation"><ul></ul></div><div id="enabled"><ul></ul></div><button id="save"></button></body></html>');
+global.window = window;
+global.document = window.document;
+global.$ = $;
 
-describe('admin/settings/navigation', function () {
-	let $, navigation, socket, ajaxify, translator, iconSelect, Benchpress, alerts;
+// Mock external dependencies
+const iconSelect = require('iconSelect');
+const Benchpress = require('benchpress');
+const alerts = require('alerts');
+const translator = require('translator');
+const ajaxify = require('ajaxify');
 
-	beforeEach(function () {
-		const dom = new JSDOM(`
-			<div id="available">
-				<ul><li class="drag-item" data-id="custom"></li></ul>
-			</div>
-			<div id="active-navigation"></div>
-			<div id="enabled">
-				<li data-index="1"><form></form></li>
-			</div>
-			<button id="save"></button>
-		`, { runScripts: 'dangerously', resources: 'usable' });
+// Stub methods
+sinon.stub(iconSelect, 'init').callsFake((iconEl, callback) => callback({ attr: () => 'new-icon-class' }));
+sinon.stub(Benchpress, 'parse').callsFake((view, template, data, callback) => callback('<li>Mocked Item</li>'));
+sinon.stub(translator, 'escape').callsFake(str => str);
+sinon.stub(translator, 'translate').callsFake((li, callback) => callback(li));
+sinon.stub(alerts, 'error');
+sinon.stub(ajaxify, 'data').value({ available: [{ groups: [] }] });
 
-		global.window = dom.window;
-		global.document = dom.window.document;
-		$ = require('jquery')(dom.window);
+describe('Navigation Module', function() {
+    beforeEach(function() {
+        // Initialize navigation module
+        navigation.init();
+    });
 
-		socket = { emit: sinon.stub() };
-		ajaxify = { data: { available: [], groups: [] } };
-		translator = { escape: sinon.stub().returnsArg(0), translate: sinon.stub(), unescape: sinon.stub().returnsArg(0) };
-		iconSelect = { init: sinon.stub() };
-		Benchpress = { parse: sinon.stub().callsFake((tpl, block, data, callback) => callback('<li></li>')) };
-		alerts = { error: sinon.stub() };
+    it('should initialize drag and drop functionality', function() {
+        // Simulate initialization and check if elements are correctly set up
+        assert.strictEqual($('#available').find('li .drag-item').length, 0);
+    });
 
-		// Require the navigation module after setting up globals
-		navigation = require(navigationModulePath)(translator, iconSelect, Benchpress, alerts);
-	});
+    it('should handle icon change', function() {
+        $('#enabled').find('.iconPicker').trigger('click');
+        // Add assertions for icon change
+    });
 
-	afterEach(function () {
-		sinon.restore();
-	});
+    it('should handle dropdown toggle', function() {
+        $('#enabled').find('[name="dropdown"]').trigger('click');
+        // Add assertions for dropdown toggle
+    });
 
-	describe('init', function () {
-		it('should initialize drag-and-drop and event handlers', function (done) {
-			async.series([
-				function (next) {
-					const draggableStub = sinon.spy($.fn, 'draggable');
-					const sortableStub = sinon.spy($.fn, 'sortable');
-					const droppableStub = sinon.spy($.fn, 'droppable');
+    it('should handle save functionality', function(done) {
+        sinon.stub(socket, 'emit').callsFake((event, data, callback) => callback(null));
+        $('#save').trigger('click');
+        // Add assertions for save functionality
+        done();
+    });
 
-					navigation.init();
-
-					assert.strictEqual(draggableStub.calledOnce, true, 'draggable should be called once');
-					assert.strictEqual(sortableStub.calledOnce, true, 'sortable should be called once');
-					assert.strictEqual(droppableStub.calledOnce, true, 'droppable should be called once');
-
-					next();
-				},
-			], done);
-		});
-
-		it('should handle iconPicker click event', function (done) {
-			async.series([
-				function (next) {
-					navigation.init();
-
-					const iconEl = $('<i class="fa fa-icon"></i>');
-					const iconPicker = $('<div class="iconPicker"></div>').append(iconEl);
-
-					$('#enabled').append(iconPicker);
-					iconPicker.click();
-
-					assert.strictEqual(iconSelect.init.calledOnce, true, 'iconSelect.init should be called once');
-					assert.strictEqual(iconSelect.init.calledWith(iconEl), true, 'iconSelect.init should be called with iconEl');
-
-					next();
-				},
-			], done);
-		});
-
-		it('should handle dropdown toggle event', function (done) {
-			async.series([
-				function (next) {
-					navigation.init();
-
-					const dropdownCheckbox = $('<input type="checkbox" name="dropdown">');
-					const enabledItem = $('<li data-index="1"></li>');
-					enabledItem.append(dropdownCheckbox);
-					$('#enabled').append(enabledItem);
-
-					dropdownCheckbox.click();
-					assert.strictEqual(dropdownCheckbox.is(':checked'), true, 'dropdown checkbox should be checked');
-
-					next();
-				},
-			], done);
-		});
-	});
-
-	describe('save', function () {
-		it('should emit save event with navigation data', function (done) {
-			async.series([
-				function (next) {
-					navigation.init();
-
-					navigation.save();
-
-					assert.strictEqual(socket.emit.calledOnce, true, 'socket.emit should be called once');
-					assert.strictEqual(socket.emit.calledWith('admin.navigation.save'), true, 'socket.emit should be called with admin.navigation.save');
-
-					next();
-				},
-			], done);
-		});
-	});
-
-	describe('remove', function () {
-		it('should remove item from active-navigation and enabled', function (done) {
-			async.series([
-				function (next) {
-					navigation.init();
-
-					const removeBtn = $('<button class="delete"></button>');
-					const enabledItem = $('<li data-index="1"></li>').append(removeBtn);
-					$('#enabled').append(enabledItem);
-
-					removeBtn.click();
-
-					assert.strictEqual($('#active-navigation [data-index="1"]').length, 0, 'Item should be removed from active-navigation');
-					assert.strictEqual($('#enabled [data-index="1"]').length, 0, 'Item should be removed from enabled');
-
-					next();
-				},
-			], done);
-		});
-	});
-
-	describe('toggle', function () {
-		it('should toggle item enabled state', function (done) {
-			async.series([
-				function (next) {
-					navigation.init();
-
-					const toggleBtn = $('<button class="toggle enable"></button>');
-					const enabledItem = $('<li data-index="1"></li>').append(toggleBtn);
-					$('#enabled').append(enabledItem);
-
-					toggleBtn.click();
-
-					assert.strictEqual(toggleBtn.hasClass('hidden'), true, 'Toggle button should be hidden');
-					assert.strictEqual($('#active-navigation [data-index="1"] a').hasClass('text-muted'), true, 'Item in active-navigation should be muted');
-
-					next();
-				},
-			], done);
-		});
-	});
+    afterEach(function() {
+        sinon.restore();
+    });
 });
