f27c39c09b705c65d584b2fbaf30f10ecc531110
reemhashir <rmuhamme@andrew.cmu.edu>
2024-09-05 13:55:21 -0400

    reworked old code to fix style errors

--- public/src/admin/settings/navigation.js
+++ public/src/admin/settings/navigation.js
@@ -1,169 +1,173 @@
 'use strict';
 
-const $ = require('jquery');
-const iconSelect = require('iconSelect');
-const Benchpress = require('benchpress');
-const alerts = require('alerts');
-const translator = require('translator');
-const ajaxify = require('ajaxify');
+define('admin/settings/navigation', [
+	'translator',
+	'iconSelect',
+	'benchpress',
+	'alerts',
+	'jquery-ui/widgets/draggable',
+	'jquery-ui/widgets/droppable',
+	'jquery-ui/widgets/sortable',
+], function (translator, iconSelect, Benchpress, alerts) {
+	const navigation = {};
+	let available;
 
-const navigation = {};
-let available;
+	navigation.init = function () {
+		available = ajaxify.data.available;
 
-navigation.init = function () {
-  available = ajaxify.data.available;
+		$('#available').find('li .drag-item').draggable({
+			connectToSortable: '#active-navigation',
+			helper: 'clone',
+			distance: 10,
+			stop: drop,
+		});
 
-  $('#available').find('li .drag-item').draggable({
-    connectToSortable: '#active-navigation',
-    helper: 'clone',
-    distance: 10,
-    stop: drop,
-  });
+		$('#active-navigation').sortable().droppable({
+			accept: $('#available li .drag-item'),
+		});
 
-  $('#active-navigation').sortable().droppable({
-    accept: $('#available li .drag-item'),
-  });
+		$('#enabled').on('click', '.iconPicker', onIconPickerClick);
+		$('#enabled').on('click', '[name="dropdown"]', onDropdownClick);
+		$('#active-navigation').on('click', 'li', onSelect);
+		$('#enabled').on('click', '.delete', remove);
+		$('#enabled').on('click', '.toggle', toggle);
+		$('#save').on('click', save);
+	};
 
-  $('#enabled').on('click', '.iconPicker', function () {
-    const iconEl = $(this).find('i');
-    iconSelect.init(iconEl, function (el) {
-      const newIconClass = el.attr('value');
-      const index = iconEl.parents('[data-index]').attr('data-index');
-      $('#active-navigation [data-index="' + index + '"] i.nav-icon').attr('class', 'fa fa-fw ' + newIconClass);
-      iconEl.siblings('[name="iconClass"]').val(newIconClass);
-      iconEl.siblings('.change-icon-link').toggleClass('hidden', !!newIconClass);
-    });
-  });
+	function onIconPickerClick() {
+		const iconEl = $(this).find('i');
+		iconSelect.init(iconEl, onIconSelect);
+	}
 
-  $('#enabled').on('click', '[name="dropdown"]', function () {
-    const el = $(this);
-    const index = el.parents('[data-index]').attr('data-index');
-    $('#active-navigation [data-index="' + index + '"] i.dropdown-icon').toggleClass('hidden', !el.is(':checked'));
-  });
+	function onIconSelect(el) {
+		const newIconClass = el.attr('value');
+		const index = el.parents('[data-index]').attr('data-index');
+		$('#active-navigation [data-index="' + index + '"] i.nav-icon').attr('class', 'fa fa-fw ' + newIconClass);
+		el.siblings('[name="iconClass"]').val(newIconClass);
+		el.siblings('.change-icon-link').toggleClass('hidden', !!newIconClass);
+	}
 
-  $('#active-navigation').on('click', 'li', onSelect);
+	function onDropdownClick() {
+		const el = $(this);
+		const index = el.parents('[data-index]').attr('data-index');
+		$('#active-navigation [data-index="' + index + '"] i.dropdown-icon').toggleClass('hidden', !el.is(':checked'));
+	}
 
-  $('#enabled')
-    .on('click', '.delete', remove)
-    .on('click', '.toggle', toggle);
+	function onSelect() {
+		const clickedIndex = $(this).attr('data-index');
+		selectIndex(clickedIndex);
+		return false;
+	}
 
-  $('#save').on('click', save);
-};
+	function selectIndex(index) {
+		$('#active-navigation li').removeClass('active');
+		$('#active-navigation [data-index="' + index + '"]').addClass('active');
 
-function onSelect() {
-  const clickedIndex = $(this).attr('data-index');
-  selectIndex(clickedIndex);
-  return false;
-}
+		const detailsForm = $('#enabled').children('[data-index="' + index + '"]');
+		$('#enabled li').addClass('hidden');
 
-function selectIndex(index) {
-  $('#active-navigation li').removeClass('active');
-  $('#active-navigation [data-index="' + index + '"]').addClass('active');
+		if (detailsForm.length) {
+			detailsForm.removeClass('hidden');
+		}
+	}
 
-  const detailsForm = $('#enabled').children('[data-index="' + index + '"]');
-  $('#enabled li').addClass('hidden');
+	function drop(ev, ui) {
+		const id = ui.helper.attr('data-id');
+		const el = $('#active-navigation [data-id="' + id + '"]');
+		const data = getData(id);
 
-  if (detailsForm.length) {
-    detailsForm.removeClass('hidden');
-  }
-}
+		data.index = getNextIndex();
+		data.title = translator.escape(data.title);
+		data.text = translator.escape(data.text);
+		data.groups = ajaxify.data.groups;
 
-function drop(ev, ui) {
-  const id = ui.helper.attr('data-id');
-  const el = $('#active-navigation [data-id="' + id + '"]');
-  const data = id === 'custom' ? {
-    iconClass: 'fa-navicon',
-    groups: available[0].groups,
-    enabled: true,
-  } : available[id];
+		Promise.all([
+			renderTemplate('admin/settings/navigation', 'navigation', { navigation: [data] }, el),
+			renderTemplate('admin/settings/navigation', 'enabled', { enabled: [data] }, $('#enabled')),
+		]).then(() => selectIndex(data.index));
+	}
 
-  data.index = (parseInt($('#enabled').children().last().attr('data-index'), 10) || 0) + 1;
-  data.title = translator.escape(data.title);
-  data.text = translator.escape(data.text);
-  data.groups = ajaxify.data.groups;
+	function getData(id) {
+		return id === 'custom' ? {
+			iconClass: 'fa-navicon',
+			groups: available[0].groups,
+			enabled: true,
+		} : available[id];
+	}
 
-  const renderNav = new Promise((resolve) => {
-    Benchpress.parse('admin/settings/navigation', 'navigation', { navigation: [data] }, function (li) {
-      translator.translate(li, function (li) {
-        li = $(translator.unescape(li));
-        el.after(li);
-        el.remove();
-        resolve();
-      });
-    });
-  });
-  const renderForm = new Promise((resolve) => {
-    Benchpress.parse('admin/settings/navigation', 'enabled', { enabled: [data] }, function (li) {
-      translator.translate(li, function (li) {
-        li = $(translator.unescape(li));
-        $('#enabled').append(li);
-        resolve();
-      });
-    });
-  });
+	function getNextIndex() {
+		return (parseInt($('#enabled').children().last().attr('data-index'), 10) || 0) + 1;
+	}
 
-  Promise.all([
-    renderNav,
-    renderForm,
-  ]).then(() => selectIndex(data.index));
-}
+	function renderTemplate(template, section, data, container) {
+		return new Promise((resolve) => {
+			Benchpress.parse(template, section, data, function (li) {
+				translator.translate(li, function (li) {
+					li = $(translator.unescape(li));
+					container.append(li);
+					resolve();
+				});
+			});
+		});
+	}
 
-function save() {
-  const nav = [];
+	function save() {
+		const nav = [];
+		const indices = [];
 
-  const indices = [];
-  $('#active-navigation li').each(function () {
-    indices.push($(this).attr('data-index'));
-  });
+		$('#active-navigation li').each(function () {
+			indices.push($(this).attr('data-index'));
+		});
 
-  indices.forEach(function (index) {
-    const el = $('#enabled').children('[data-index="' + index + '"]');
-    const form = el.find('form').serializeArray();
-    const data = {};
+		indices.forEach(function (index) {
+			const el = $('#enabled').children('[data-index="' + index + '"]');
+			const form = el.find('form').serializeArray();
+			const data = {};
 
-    form.forEach(function (input) {
-      if (data[input.name]) {
-        if (!Array.isArray(data[input.name])) {
-          data[input.name] = [data[input.name]];
-        }
-        data[input.name].push(input.value);
-      } else {
-        data[input.name] = input.value;
-      }
-    });
+			form.forEach(function (input) {
+				if (data[input.name]) {
+					if (!Array.isArray(data[input.name])) {
+						data[input.name] = [data[input.name]];
+					}
+					data[input.name].push(input.value);
+				} else {
+					data[input.name] = input.value;
+				}
+			});
 
-    nav.push(data);
-  });
+			nav.push(data);
+		});
 
-  socket.emit('admin.navigation.save', nav, function (err) {
-    if (err) {
-      alerts.error(err);
-    } else {
-      const saveBtn = document.getElementById('save');
-      saveBtn.classList.toggle('saved', true);
-      setTimeout(() => {
-        saveBtn.classList.toggle('saved', false);
-      }, 5000);
-    }
-  });
-}
+		socket.emit('admin.navigation.save', nav, function (err) {
+			if (err) {
+				alerts.error(err);
+			} else {
+				const saveBtn = document.getElementById('save');
+				saveBtn.classList.toggle('saved', true);
+				setTimeout(() => {
+					saveBtn.classList.toggle('saved', false);
+				}, 5000);
+			}
+		});
+	}
 
-function remove() {
-  const index = $(this).parents('[data-index]').attr('data-index');
-  $('#active-navigation [data-index="' + index + '"]').remove();
-  $('#enabled [data-index="' + index + '"]').remove();
-  return false;
-}
+	function remove() {
+		const index = $(this).parents('[data-index]').attr('data-index');
+		$('#active-navigation [data-index="' + index + '"]').remove();
+		$('#enabled [data-index="' + index + '"]').remove();
+		return false;
+	}
 
-function toggle() {
-  const btn = $(this);
-  const disabled = btn.hasClass('enable');
-  const index = btn.parents('[data-index]').attr('data-index');
-  btn.siblings('.toggle').removeClass('hidden');
-  btn.addClass('hidden');
-  btn.parents('li').find('[name="enabled"]').val(disabled ? 'on' : '');
-  $('#active-navigation [data-index="' + index + '"] a').toggleClass('text-muted', !disabled);
-  return false;
-}
+	function toggle() {
+		const btn = $(this);
+		const disabled = btn.hasClass('enable');
+		const index = btn.parents('[data-index]').attr('data-index');
+		btn.siblings('.toggle').removeClass('hidden');
+		btn.addClass('hidden');
+		btn.parents('li').find('[name="enabled"]').val(disabled ? 'on' : '');
+		$('#active-navigation [data-index="' + index + '"] a').toggleClass('text-muted', !disabled);
+		return false;
+	}
 
-module.exports = navigation;
+	return navigation;
+});
